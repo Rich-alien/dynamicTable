7bf33de92a5cd7150449a5e33d90af3fc2963add
sh <sh@nekta.tech>
2022-08-19 01:27:01 +0400

    Table data and preprocessing

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,30 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {ProductService} from "../services/product.service";
+import {PRODUCT_TABLE_HEADER} from "../consts/const";
+import {TableType} from "../models/table.model";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.less']
 })
 export class AppComponent {
-  title = 'testTask';
+  public table: TableType = {
+    header: PRODUCT_TABLE_HEADER,
+    data: []
+  };
+  public isReloaded = false;
+
+  constructor(private productService: ProductService) {
+  }
+
+  ngOnInit(): void {
+    this.productService.getProductList().subscribe(
+      (product) => {
+        this.table.data = product;
+        this.isReloaded = true;
+      }
+    )
+  }
+
 }
